df2f2939261351e414c3efe5e19d791d91469c88
Rami H <dv.rami@pm.me>
2019-07-07 15:22:42 -0400

    created client and server pb stubs

---
+++ protogen.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for x in rpc/**/*.proto; do protoc --twirp_out=paths=source_relative:. --go_out=paths=source_relative:. $x; done
+
+# Generate self signed Certificate for TLS communication
+# openssl req \
+#        -newkey rsa:2048 -nodes -keyout private.key \
+#        -x509 -days 365 -out cert.crt
