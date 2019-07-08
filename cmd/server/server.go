610b19efe1d3452761e4c1c0817519bea6c79100
Rami H <dv.rami@pm.me>
2019-07-07 22:15:30 -0400

    twirp services mounted on server

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"net/http"
+	"github.com/rumsrami/givly-rpc-api/internal"
+	"github.com/gorilla/mux"
+)
+
 func main() {
-	
+	r := mux.NewRouter()
+
+	merchantHandler := internal.NewMerchantDirectoryHandler()
+	transactionHandler := internal.NewTransactionDirectoryHandler()
+
+	r.Handle("/api/merchant", merchantHandler)
+	r.Handle("/api/transaction", transactionHandler)
+
+	http.ListenAndServe(":8080", r)
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.12
 
 require (
 	github.com/golang/protobuf v1.3.1
+	github.com/gorilla/mux v1.7.3
 	github.com/twitchtv/twirp v5.7.0+incompatible
 )
